2a3e6ba45e374825722ad2dc3eab8152f0735a0e
Claire REMY <anha.flint@gmail.com>
2015-04-01 20:58:04 +0200

    remove server instanciation

--- src/router.js
+++ src/router.js
@@ -12,30 +12,10 @@ var crawler = require('./Crawler');
 var args = process.argv.slice(2);
 
 var machine = new Machine();
-crawler.ask();
+crawler.ask(machine);
 
 if(args[0] === 'client') {
-	// instantiate new client machine
-	var socket = new net.Socket({
-			readable: true,
-			writable: true
-		});
-	socket.connect(8124);
-	if(typeof args[1] !== 'undefined') {
-		socket.write(args[1]);
-	}
+	machine.setClient();
 } else if (args[0] === 'router') {
 	machine.setRouter();
-	// instantiate new router machine
-	var server = net.createServer(function(c) {
-		c.on('data', function(data) {
-			console.log('data ' + c.remoteAddress + ' : ' + data);
-			c.write("you said " + data) ;
-		});
-	}); 
-	server.listen(8124, function() {
-		
-	});
 }
-
-
