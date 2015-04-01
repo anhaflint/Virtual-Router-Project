a55007566281eb7b0d7770b658e9594a84a9db2a
Claire REMY <anha.flint@gmail.com>
2015-04-01 20:57:41 +0200

    update methods for NVR

--- src/Interface.js
+++ src/Interface.js
@@ -1,25 +1,52 @@
+var net = require('net');
+
 //Constructeur de la classe Interface
-module.exports = function Interface(nom) {
-	this.nom = nom;
+module.exports = function Interface(name, ip) {
+	this.name = name;
 	this.ip = "";
+	this.server = null;
 	this.mode = "down";
-	console.log("nouvelle interface créée");
+
+	
 
 	this.setIP = function(ip) {
 		this.ip = ip;
-		console.log("IP set for " + this.nom + " is : " + this.ip);
+		console.log("IP set for " + this.name + " is : " + this.ip);
 	}
 
 	this.flushIP = function() {
 		this.ip = "";
 	}
 
-	this.setUP = function () {
-		this.mode = "up" ;
+	this.set = function (mode) {
+		this.mode = mode ;
 	}
 
-	this.setDown = function() {
-		this.mode = "down";
+	this.print = function() {
+		console.log(
+			'{ \n name : '
+			+ this.name
+			+ '\n'
+			+ ' ip : '
+			+ this.ip
+			+ '\n'
+			+ ' mode : ' 
+			+ this.mode
+			+ '\n}'
+			)
 	}
 
+	if (typeof ip !== 'undefined') 
+	{
+		this.setIP(ip);
+		this.server = net.createServer(function(c) {
+			c.on('data', function(data) {
+				console.log('data ' + c.remoteAddress + ' : ' + data);
+			});
+		}); 
+		this.server.listen(this.ip, function() {
+			console.log("interface up");
+		});
+		this.set("up");
+	}
 }
--- src/Machine.js
+++ src/Machine.js
@@ -1,22 +1,59 @@
 //Constructeur de la classe Machine
 var Interface = require('./Interface.js');
+var net = require('net');
 
 module.exports = function Machine() {
 	this.mode = "client";
-	this.nom = "";
+	this.name = "";
 	this.interfaces = [];
 	this.tabAddr = [];
+	this.socket = this.socket = new net.Socket({
+			readable: true,
+			writable: true
+		});
 
-	this.setNom = function(nom) {
-		this.nom = nom;
+	this.setName = function(name) {
+		this.name = name;
 	}
 
-	this.addInterface = function(nom) {
-		this.interfaces.push(new Interface(nom));
+	this.addInterface = function(name, ip) {
+		if(typeof ip !== 'undefined') {
+			this.interfaces.push(new Interface(name, ip));
+		}
+		this.printInterfaces();
+	}
+
+	this.getInterface = function(name) {
+		for(var i in this.interfaces) {
+			if(this.interfaces[i].name == name) 
+			{
+				return this.interfaces[i];
+			} 
+		}
+		console.log('ERROR : interface does not exist');
+		this.printInterfaces();
+	}
+
+	this.printInterfaces = function() {
+		for(var i in this.interfaces) 
+		{
+			this.interfaces[i].print();
+		}
 	}
 
 	this.setRouter = function() {
 		this.mode = "router";
 	}
 
+	this.setClient = function() {
+		this.mode = "client";
+	}
+
+	this.connect = function(ip, message) {
+		this.socket.connect(ip);
+		console.log(message);
+		if(typeof message !== 'undefined') {
+			this.socket.write(message);
+		}
+	}
 }
